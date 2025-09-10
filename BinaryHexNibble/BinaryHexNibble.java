1998705efa08a94a040a24f7ff37f2debea11cf4
Alex Roberts <119014375+SlashAlexx@users.noreply.github.com>
2025-09-10 11:28:53 -0500

    Initial Commit

---
+++ BinaryHexNibble/BinaryHexNibble.java
@@ -0,0 +1,9 @@
+// Application to test users over Binary/Hex Nibble Conversion
+// Alex Roberts
+
+class BinaryHexNibble {
+
+    public static void main(String args[]){
+        System.out.println("Hello Java!");
+    }
+}
